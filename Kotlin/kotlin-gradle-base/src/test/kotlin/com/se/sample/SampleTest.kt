7807f35e185073e18fff8b6d0a05e7391ddaeb47
evgeniy.skiba <skiba.evgeniy@gmail.com>
2018-07-24 10:49:35 +0300

    added the next gradle solution

---
+++ Kotlin/kotlin-gradle-base/src/test/kotlin/com/se/sample/SampleTest.kt
@@ -0,0 +1,12 @@
+package com.se.sample
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+    @Test
+    fun sample() {
+        println("SampleTest");
+        assertEquals("Gradle is gr8", "Gradle is gr8")
+    }
+}
